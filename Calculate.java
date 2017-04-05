b51305c34873f05b613a36f42b964e5d0fb14cde
ConstantBel <constant_bel@ukr.net>
2017-04-05 17:53:38 +0300

    update 3

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,15 @@ public class Calculate {
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
 		int summ = first + second;
-		System.out.println("Sum  " + summ);
+		int multiplication = first * second;
+		int subtraction = first - second;
+		int division = first / second;
+		
+		System.out.println("Sum  " + summ); 
+		System.out.println("*  " + multiplication);
+		System.out.println("-  " + subtraction);
+		System.out.println("/  " + division);
+		System.out.println("/  " + division);
+		 
 	}
 }
